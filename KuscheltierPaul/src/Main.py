e9a695ae6cd09980cd69f7ec3a4b369659028b74
Christoph <ckern@student.tgm.ac.at>
2018-12-05 17:09:14 +0100

    update

--- KuscheltierPaul/src/Main.py
+++ KuscheltierPaul/src/Main.py
@@ -1,8 +1,8 @@
-from hedgehog.client.sync_client import HedgehogClient
-from Medikamente import Medikamente
-from Termine import Termine
+from hedgehog.client import connect
+#from Medikamente import Medikamente
+#from Termine import Termine
 from SimonSays import SimonSays
-from Buecher import Buecher
+#from Buecher import Buecher
 from Puls import Puls
 import psycopg2
 from Power import Power
@@ -11,39 +11,40 @@ from Power import Power
 class Main(object):
 
     #Hier werden die Sensoren und Buttons inizialisiert
-    rHand = HedgehogClient.get_digital(1)
-    lHand = HedgehogClient.get_digital(2)
-    lFuss = HedgehogClient.get_digital(3)
-    rFuss = HedgehogClient.get_digital(4)
-    lOhr = HedgehogClient.get_digital(5)
-    pulsSanalog = HedgehogClient.get_analog(5)
-    abbr = HedgehogClient.get_digital(6)
-    notfall = HedgehogClient.get_digital(7)
+    with connect() as hedgehog:
+        rHand = hedgehog.get_digital(1)
+        lHand = hedgehog.get_digital(2)
+        lFuss = hedgehog.get_digital(3)
+        rFuss = hedgehog.get_digital(4)
+        lOhr = hedgehog.get_digital(5)
+        pulsSanalog = hedgehog.get_analog(5)
+        abbr = hedgehog.get_digital(6)
+        notfall = hedgehog.get_digital(7)
 
     # Hier wird die PostgreSQL Verbindung inizialisiert
-    conn = psycopg2.connect("dbname=paul user=Vinc password=Vinc")
+    conn = psycopg2.connect("dbname=paul user=vinc password=vinc")
 
     # Hier werden Objekte der benötigten Klassen erstellt
-    m = Medikamente(conn)
-    t = Termine(conn,rFuss)
+    #m = Medikamente(conn)
+    #t = Termine(conn,rFuss)
     s = SimonSays(conn,rHand,lHand,rFuss,lFuss, abbr, notfall)
-    b = Buecher(conn, rHand)
+    #b = Buecher(conn, rHand)
     p = Puls(lOhr, pulsSanalog)
-    pwr = Power()
+    #pwr = Power()
 
     #def thread():
         #pass
     # Die Methode ruft alle benötigten Funktionen der Klassen auf
     def callFunctions(self):
-        self.pwr.getPower()
+        #self.pwr.getPower()
         self.s.getSimonSays()
-        self.b.getBuecher()
+        #self.b.getBuecher()
         self.p.getPuls()
         #thread = Process(target=f, args=('bob',))
         #thread.start()
-        while True:
-            self.m.getMedikamente()
-            self.t.getTermine()
+        #while True:
+            #self.m.getMedikamente()
+            #self.t.getTermine()
 
 
 # Hier wird die Methode callFunctions ausgeführt.
--- KuscheltierPaul/src/SimonSays.py
+++ KuscheltierPaul/src/SimonSays.py
@@ -16,95 +16,99 @@ engine = pyttsx3.init()
 # die Worte "Simon sagt " vor der Aktion gesagt wurden.
 class SimonSays(object):
 
+        if(platform.system()=='Windows'):
+            deutsch = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Speech\Voices\Tokens\eSpeak_3"
+            engine.setProperty('rate', 100)
+        else:
+            deutsch = "german"
+            engine.setProperty('rate', 140)
+            engine.setProperty('volume',20)
+        engine.setProperty('voice', deutsch)
+
 	# Hier werden die Variablen aus dem Parameter der Klasse initialisiert
-	def __init__(self, conn,rHand,lHand,rFuss,lFuss, abbr, notfall):
-		self.conn = conn
-		self.rHand = rHand
-		self.lHand = lHand
-		self.rFuss = rFuss
-		self.lFuss = lFuss
-		self.abbr = abbr
-		self.notfall = notfall
+        def __init__(self, conn,rHand,lHand,rFuss,lFuss, abbr, notfall):
+            self.conn = conn
+            self.rHand = rHand
+            self.lHand = lHand
+	    self.rFuss = rFuss
+	    self.lFuss = lFuss
+	    self.abbr = abbr
+	    self.notfall = notfall
 
 	# Diese Methode gibt in zufälliger Reihenfolge die Ausgabe wieder.
 	def getAusgabe(self):
-		ausgabe = ""
-		zahl = random.randrange(1,9)
-		if zahl == 1:
-			ausgabe = "Drücke meine rechte Hand"
-		elif zahl == 2:
-			ausgabe = "Drücke meine linke Hand"
-		elif zahl == 3:
-			ausgabe = "Drücke meinen rechten Fuß"
-		elif zahl == 4:
-			ausgabe = "Drücke meinen linken Fuß"
-		elif zahl == 5:
-			ausgabe = "Simon sagt drücke meine rechte Hand"
-		elif zahl == 6:
-			ausgabe = "Simon sagt drücke meine linke Hand"
-		elif zahl == 7:
-			ausgabe = "Simon sagt drücke meinen rechten Fuß"
-		elif zahl == 8:
-			ausgabe = "Simon sagt drücke meinen linken Fuß"
-		return {
-			"ausgabe": ausgabe,
-			"zahl": zahl
-		}
+	    ausgabe = ""
+	    zahl = random.randrange(1,9)
+	    if zahl == 1:
+	    	ausgabe = "Drücke meine rechte Hand"
+	    elif zahl == 2:
+	    	ausgabe = "Drücke meine linke Hand"
+	    elif zahl == 3:
+	    	ausgabe = "Drücke meinen rechten Fuß"
+	    elif zahl == 4:
+	    	ausgabe = "Drücke meinen linken Fuß"
+	    elif zahl == 5:
+	    	ausgabe = "Simon sagt drücke meine rechte Hand"
+	    elif zahl == 6:
+	    	ausgabe = "Simon sagt drücke meine linke Hand"
+	    elif zahl == 7:
+	    	ausgabe = "Simon sagt drücke meinen rechten Fuß"
+	    elif zahl == 8:
+	    	ausgabe = "Simon sagt drücke meinen linken Fuß"
+	    return {
+	    	"ausgabe": ausgabe,
+	    	"zahl": zahl
+            }
 
 
 	# Diese Methode identifiziert das Drücken der Knöpfe als Zahl. Wenn z.B. die rechte Hand gedrückt wird, dann wird die Zahl 5 zurück gegeben.
 	# Diese Methode wird dafür verwendet, um einen Knopfdruck mit der obig genannten Methode ab zu gleichen. Wenn die Zahl der parseInputs Methode
 	# der Zahl der getAusgabe Methode entspricht, ist der gedrückte Knopf richtig.
-	def parseInputs(self):
-		if self.rHand:
-			return 5
-		if self.lHand:
-			return 6
-		if self.rFuss:
-			return 7
-		if self.lFuss:
-			return  8
-		return 0
-
-
-
-
-
-	def falscheAusgabe(self,score):
-			engine.say('Das war leider falsch.')
-			engine.runAndWait()
-			engine.say('Dein Score beträgt '+str(score))
-			engine.runAndWait()
+        def parseInputs(self):
+	    if self.rHand:
+	    	return 5
+	    if self.lHand:
+	    	return 6
+	    if self.rFuss:
+	    	return 7
+	    if self.lFuss:
+	    	return  8
+	    return 0
+        def falscheAusgabe(self,score):
+	    engine.say('Das war leider falsch.')
+	    engine.runAndWait()
+	    engine.say('Dein Score beträgt '+str(score))
+	    engine.runAndWait()
 
 	# Diese Methode verwendet die obig genannten Methoden und generiert das Spiel Simon Says.
 	# Die Methode überprüft die Zahlen der Methode getAusgabe und vergleicht sie mit den Zahlen der Methode parseInputs
 	# Hier befindet sich die Logik des Spiels.
-	def getSimonSays(self):
-		engine.say("Sie haben das Spiel Simon Says gewählt")
-		engine.runAndWait()
-		score = 0
-		while self.notfall == False and self.abbr == False:
-			ausgabe = self.getAusgabe()
-			engine.say(ausgabe['ausgabe'])
-			engine.runAndWait()
-			tend = time.time()+5
-			while time.time() < tend:
-				if ausgabe['zahl'] <= 4 and self.parseInputs() > 0:
-					engine.say('Das war leider falsch.')
-					engine.runAndWait()
-					engine.say('Dein Score beträgt '+str(score))
-					engine.runAndWait()
+        def getSimonSays(self):
+	    engine.say("Sie haben das Spiel Simon Says gewählt")
+	    engine.runAndWait()
+	    score = 0
+	    while self.notfall == False and self.abbr == False:
+	    	ausgabe = self.getAusgabe()
+	    	engine.say(ausgabe['ausgabe'])
+	    	engine.runAndWait()
+	    	tend = time.time()+5
+	    	while time.time() < tend:
+	    		if ausgabe['zahl'] <= 4 and self.parseInputs() > 0:
+	    			engine.say('Das war leider falsch.')
+	    			engine.runAndWait()
+	    			engine.say('Dein Score beträgt '+str(score))
+	    			engine.runAndWait()
 
-				elif (ausgabe['zahl'] <= 4) and (self.parseInputs() == 0):
-					engine.say('richtig.')
-					engine.runAndWait()
-				elif ausgabe['zahl'] == self.parseInputs():
-					engine.say('richtig')
-					engine.runAndWait()
-					score += 1
-				else:
-					self.falscheAusgabe(score)
+			    elif (ausgabe['zahl'] <= 4) and (self.parseInputs() == 0):
+			    	engine.say('richtig.')
+			    	engine.runAndWait()
+			    elif ausgabe['zahl'] == self.parseInputs():
+			    	engine.say('richtig')
+			    	engine.runAndWait()
+			    	score += 1
+			    else:
+			    	self.falscheAusgabe(score)
 
-		cur = self.conn.cursor()
-		cur.execute("INSERT INTO score VALUES(%s)", (score,))
-		self.conn.commit
+	    cur = self.conn.cursor()
+	    cur.execute("INSERT INTO score VALUES(%s)", (score,))
+	    self.conn.commit
